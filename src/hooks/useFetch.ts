ffd75949d01da091f44b6bf2bd419b71bcf25aaa
xulk <690978247@qq.com>
2024-05-29 15:15:36 +0800

    feat: watchEffect, toValue

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,23 @@
+import { ref, toValue, watchEffect } from 'vue'
+
+export function useFetch(url: string) {
+  const data = ref(null)
+  const error = ref(null)
+
+  const fetchData = () => {
+    // reset state before fetching..
+    data.value = null
+    error.value = null
+
+    fetch(toValue(url))
+      .then((res) => res.json())
+      .then((json) => (data.value = json))
+      .catch((err) => (error.value = err))
+  }
+
+  watchEffect(() => {
+    fetchData()
+  })
+
+  return { data, error }
+}
--- src/views/vue3-study/combineFun/index.vue
+++ src/views/vue3-study/combineFun/index.vue
@@ -1,9 +1,49 @@
 <template>
   <div>
     <p>组合式函数</p>
+    <p>
+      Load post id:
+      <button v-for="i in 5" @click="id = `${i}`">{{ i }}</button>
+
+      <el-input v-model="inputVal" ></el-input>
+
+      <div v-if="error" >
+        <p>Oops! Error encountered: {{ error.message }}</p>
+        <button @click="retry">Retry</button>
+      </div>
+
+      <div v-else-if="data"> Data Loaded: <pre>{{ data }}</pre> </div>
+      <div v-else >Loading...</div>
+    </p>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, computed, toValue } from 'vue'
+import { useFetch } from '@/hooks/useFetch'
+import { watchEffect } from 'vue';
+
+const baseUrl = 'https://jsonplaceholder.typicode.com/todos/'
+const id = ref('1')
+const inputVal = ref('')
+const url = computed(() => baseUrl + id.value)
+
+const testid = toValue(id)
+
+const { data, error } = useFetch(url)
+
+console.log('iiii', toValue(id))
+
+const retry = () => {
+  id.value = ''
+  id.value = '1'
+}
+
+watchEffect(() => {
+  console.log('mmmm', toValue(inputVal))
+  console.log('wwwww',toValue(id), testid)
+})
+
+</script>
 
 <style lang="scss" scoped></style>
