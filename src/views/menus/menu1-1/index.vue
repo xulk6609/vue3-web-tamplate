b81fb954f5df268c0ced0bf624b167cdc292897d
xulk <690978247@qq.com>
2024-10-14 16:51:48 +0800

    feat: 还原菜单栏 数据

--- src/views/menus/menu1-1/index.vue
+++ src/views/menus/menu1-1/index.vue
@@ -1,45 +1,5 @@
 <template>
-  <swiper-container
-    :slides-per-view="3"
-    :space-between="spaceBetween"
-    :centered-slides="true"
-    :pagination="{
-      hideOnClick: true
-    }"
-    :breakpoints="{
-      768: {
-        slidesPerView: 3
-      }
-    }"
-    :loop="true"
-    :autoplay="{
-      delay: 2500,
-      disableOnInteraction: false,
-      pauseOnMouseEnter: true
-    }"
-    @swiperrealindexchange="onSlideChange"
-  >
-    <swiper-slide>Slide 1</swiper-slide>
-    <swiper-slide>Slide 2</swiper-slide>
-    <swiper-slide>Slide 3</swiper-slide>
-  </swiper-container>
+  <div>menu1-1</div>
 </template>
 
-<script setup lang="ts">
-import { ref } from 'vue'
-import { register } from 'swiper/element/bundle'
-
-register()
-
-const spaceBetween = ref(10)
-// const onProgress = (e) => {
-//   const [swiper, progress] = e.detail
-//   console.log(progress)
-// }
-const onSlideChange = (e) => {
-  const [swiper, progress] = e.detail
-  // const [swiper, progress] = e.detail
-  // console.log('slide changed', swiper.activeIndex)
-  console.log('9989', swiper.realIndex)
-}
-</script>
+<script setup lang="ts"></script>
--- src/views/menus/menu2-1/index.vue
+++ src/views/menus/menu2-1/index.vue
@@ -1,76 +1,7 @@
 <template>
-  <div class="carousel-swiper">
-    <winnerCarousel :data="rightData" ref="carousel" />
-  </div>
+  <div>menu2-1</div>
 </template>
 
-<script setup lang="ts">
-import { onMounted, ref } from 'vue'
-import winnerCarousel from '@/components/carousel/top100Carousel.vue'
-import testImg from '@/assets/images/home/test2.png'
+<script setup lang="ts"></script>
 
-// Import Swiper Vue.js components
-// import { SwiperSlide } from 'swiper/vue'
-
-// Import Swiper styles
-// import 'swiper/css'
-// import 'swiper/css/effect-cards'
-
-onMounted(() => {
-  setTimeout(() => {
-    rightData.value = [
-      {
-        mainImage: testImg,
-        title: 'Vestido Pin Up Midi Bolinha Rodado Alca de Amarrar 111',
-        count: '100w+'
-      },
-      {
-        mainImage: testImg,
-        title: 'Vestido Pin Up Midi Bolinha Rodado Alca de Amarrar 222',
-        count: '200w+'
-      },
-      {
-        mainImage: testImg,
-        title: 'Vestido Pin Up Midi Bolinha Rodado Alca de Amarrar 333',
-        count: '300w+'
-      }
-    ]
-  }, 300)
-})
-
-const rightData = ref([])
-</script>
-
-<style lang="scss" scoped>
-// // 局部引入，防止样式污染
-// @import 'swiper/css';
-// @import 'swiper/css/effect-cards';
-.carousel-swiper {
-  width: 500px;
-  height: 500px;
-}
-
-// .swiper-slide {
-//   display: flex;
-//   align-items: center;
-//   justify-content: center;
-//   border-radius: 18px;
-//   font-size: 22px;
-//   width: 240px;
-//   height: 320px;
-//   font-weight: bold;
-//   color: #fff;
-// }
-
-// .swiper-slide:nth-child(1n) {
-//   background-color: rgb(206, 17, 17);
-// }
-
-// .swiper-slide:nth-child(2n) {
-//   background-color: rgb(0, 140, 255);
-// }
-
-// .swiper-slide:nth-child(3n) {
-//   background-color: rgb(10, 184, 111);
-// }
-</style>
+<style lang="scss" scoped></style>
