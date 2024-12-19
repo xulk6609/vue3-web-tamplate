572c4caaf8a6d81c1b50159ab3416b8766c6b7df
xulk <690978247@qq.com>
2024-12-19 15:39:14 +0800

    feat: 移除注释

--- src/layout/header/components/externalLink.vue
+++ src/layout/header/components/externalLink.vue
@@ -1,11 +1,3 @@
-<!--
- * @Author: 闫闪闪 3303009278@qq.com
- * @Date: 2024-02-22 10:31:53
- * @LastEditors: 闫闪闪 3303009278@qq.com
- * @LastEditTime: 2024-03-08 14:02:03
- * @FilePath: \ADS\ads\src\layout\header\components\externalLink.vue
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
--->
 <template>
   <div class="link">
     <div
--- src/views/menus/menu1-1/index.vue
+++ src/views/menus/menu1-1/index.vue
@@ -2,4 +2,14 @@
   <div>menu1-1</div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+var arr = [1, 2, 3, { name: '111', arr: [789] }]
+var arr2 = arr.slice()
+var arr3 = arr.concat()
+
+arr3[3].arr = 888
+
+console.log(arr)
+console.log(arr2)
+console.log(arr3)
+</script>
